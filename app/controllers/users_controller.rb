58dd953f4d7ef97c9f504cbe2e2432a34f597289
yungcheng <yungcheng111023@gmail.com>
2018-01-28 21:46:39 +0800

    set root to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @users = User.find(params[:id])
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 
 10.times do |i|
   user = User.create!(name: "user#{i+1}")
